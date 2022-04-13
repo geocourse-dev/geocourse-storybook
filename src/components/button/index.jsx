008baa2deb68e99881a852e5e7d64d026ddf3b6f
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-04-13 08:42:11 +0700

    create button component

---
+++ src/components/button/index.jsx
@@ -0,0 +1,44 @@
+const Button = ({ children, variant, className, textClassName, ...props }) => {
+  const defaultClassName = `py-2 px-4 rounded-lg ${className ?? ''}`
+
+  switch (variant) {
+    case 'primary':
+      return (
+        <button
+          className={`bg-primary border-2 border-primary ${defaultClassName}`}
+          {...props}
+        >
+          <p className="text-white font-bold">{children}</p>
+        </button>
+      )
+
+    case 'secondary':
+      return (
+        <button
+          className={`bg-.. border-2 border-.. ${defaultClassName}`}
+          {...props}
+        >
+          <p className="text-white font-bold">{children}</p>
+        </button>
+      )
+
+    case 'disable':
+      return (
+        <button
+          className={`border-2 border-gray-400 ${defaultClassName}`}
+          {...props}
+        >
+          <p className="text-gray-400 font-bold">{children}</p>
+        </button>
+      )
+
+    default:
+      return (
+        <button className={defaultClassName} {...props}>
+          <p className={textClassName}>{children}</p>
+        </button>
+      )
+  }
+}
+
+export default Button
