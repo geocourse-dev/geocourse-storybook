901c996e7c77383e19dc9597dc272ccd49f6780b
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-04-13 08:54:28 +0700

    fix config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
+import '!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css'
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
