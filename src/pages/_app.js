14dbf2deea0c2bf56fb28616c35c8a0e9968fe25
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-04-16 10:09:15 +0700

    move to src

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 // import global css
-import '../src/styles/globals.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
-import { Button } from '../src/components'
+import { Button, Card } from '../components'
 
 export default function Home() {
   return (
     <>
       <h1>test komponen storybook</h1>
       <Button variant="primary">Test </Button>
+      <Card
+        title="Google Earth Engine untuk Pemula"
+        TextButton="Lihat Course"
+      />
     </>
   )
 }
