0edad21f50aed9402d387dbba9312b0774fc6c79
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-04-20 09:25:17 +0700

    move social to components

--- src/components/social/index.jsx
+++ src/components/social/index.jsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 
-export default function Social({ icon, link }) {
+export default function Social({ icon, link, width, height }) {
   return (
     <Image
       src={`/assets/social/${icon}.svg`}
-      width={30}
-      height={30}
+      width={width}
+      height={height}
       alt={`logo ${icon}`}
       onClick={() => window.open(`${link}`, '_blank')}
       className="cursor-pointer"
