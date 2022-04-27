a221872e841911023c84e53d36fffae870e4ef8f
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-04-27 13:41:42 +0700

    export cardblog component

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from './button'
 import Card from './card'
 import Footer from './footer'
 import Contact from './contact'
+import CardBlog from './cardBlog'
 
-export { Button, Card, Footer, Contact }
+export { Button, Card, Footer, Contact, CardBlog }
